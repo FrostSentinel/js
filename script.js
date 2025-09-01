ec1acd8b3704a4d80b4bd668d01c723dd82da70b
Vaibhav <frostbeaker@gmail.com>
2025-09-01 17:35:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let a = prompt("Enter first number");
+let b = prompt("Enter second number");
+let c = prompt("Enter third number");
+
+let sum =  parseInt(a) + parseInt(b) + parseInt(c);
+
+console.log("The sum is " + sum);   
